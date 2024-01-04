ac840355838b24b5da3a12d69749c8bae06d35af
Vineeth-Kolichal <vineethchandran5898@gmail.com>
2024-01-04 19:17:09 +0530

    Employee screen file created

---
+++ lib/features/employees/presentation/bloc/employees_bloc.dart
@@ -0,0 +1,13 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+
+part 'employees_event.dart';
+part 'employees_state.dart';
+
+class EmployeesBloc extends Bloc<EmployeesEvent, EmployeesState> {
+  EmployeesBloc() : super(EmployeesInitial()) {
+    on<EmployeesEvent>((event, emit) {
+      // TODO: implement event handler
+    });
+  }
+}
---
+++ lib/features/employees/presentation/pages/employees_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class EmployeesScreen extends StatelessWidget {
+  const EmployeesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Employees"),
+      ),
+    );
+  }
+}
