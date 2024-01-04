62dc9e3d046f34a3274e0765c2fcb41bb8f815a4
Vineeth-Kolichal <vineethchandran5898@gmail.com>
2024-01-04 20:02:49 +0530

    Custom scroll view implmented >image section added >button row added >sliver list added >appbar created

--- lib/features/employees/presentation/pages/employees_screen.dart
+++ lib/features/employees/presentation/pages/employees_screen.dart
@@ -5,9 +5,75 @@ class EmployeesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
     return Scaffold(
       appBar: AppBar(
-        title: Text("Employees"),
+        backgroundColor: theme.primaryColor,
+        centerTitle: true,
+        title: Text(
+          "Employees",
+          style: theme.textTheme.titleLarge!
+              .copyWith(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: CustomScrollView(
+        slivers: <Widget>[
+          //2
+          SliverAppBar(
+            expandedHeight: 250.0,
+            pinned: true,
+            floating: true,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Image.asset(
+                "assets/images/employees.jpg",
+                fit: BoxFit.cover,
+              ),
+            ),
+            bottom: PreferredSize(
+                preferredSize: Size.fromHeight(70),
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  height: 70,
+                  color: Colors.white,
+                  child: Row(
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 7),
+                        child: Container(
+                          padding: EdgeInsets.symmetric(horizontal: 10),
+                          height: 35,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(7),
+                              color: Colors.blue.withOpacity(0.8)),
+                          constraints: BoxConstraints(minWidth: 80),
+                          child: Center(
+                              child: Text(
+                            "All",
+                            style: theme.textTheme.labelLarge,
+                          )),
+                        ),
+                      )
+                    ],
+                  ),
+                )),
+          ),
+
+          //3
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (_, int index) {
+                return ListTile(
+                  leading: Container(
+                      padding: EdgeInsets.all(8),
+                      width: 100,
+                      child: Placeholder()),
+                  title: Text('Place ${index + 1}', textScaleFactor: 2),
+                );
+              },
+              childCount: 20,
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Toobler machine test',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        textTheme:
+            TextTheme(labelLarge: TextStyle(fontSize: 16, color: Colors.white)),
+        primaryColor: Colors.blue,
         // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
